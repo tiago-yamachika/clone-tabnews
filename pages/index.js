6e814ba609c7b7cfa08779ff3e147b7a7a043a99
tiago-yamachika <159552579+tiago-yamachika@users.noreply.github.com>
2025-11-13 23:46:00 +0000

    modifica o emoji no final da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Pam, se voce me ama, dá uma risadinha (e para de soltar pum dormindo =D)</h1>
+    return <h1>Pam, se voce me ama, dá uma risadinha (e para de soltar pum dormindo 😂)</h1>
 }
 
 export default Home;
