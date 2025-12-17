1183a91c80eebe34cafb54bbd347d938155ad737
tiago-yamachika <159552579+tiago-yamachika@users.noreply.github.com>
2025-12-17 15:52:48 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
-async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+async function status(_request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
   console.log(result.rows);
   response.status(200).json({ chave: "em construção" });
 }
